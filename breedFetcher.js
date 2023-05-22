a7c42939b9809722c59d8c09449db007c6dfa9ed
Vicky John <vicky.d.john@email.com>
2023-05-22 14:17:59 -0700

    Added functionality to take in breed name as command line arg.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,33 @@
 const request = require('request');
 
-//API endpoint URL
-const apiUrl = 'https://api.thecatapi.com/v1/breeds/search?q=sibe';
+// API endpoint URL
+const apiUrl = 'https://api.thecatapi.com/v1/breeds/search';
 
-//function to GET data from URL
-const breedfetcher = () => {
-  request(apiUrl, (error, response, body) => {
+// Function to fetch breed data from API
+const breedFetcher = (breedName) => {
+  const url = `${apiUrl}?q=${breedName}`;
+
+  request(url, (error, response, body) => {
     if (error) {
       console.log('Request failed');
     } else if (response.statusCode !== 200) {
       console.log(`Request failed with status code: ${response.statusCode}`);
     } else {
-      console.log(body);
+      const data = JSON.parse(body);
+      //if there is no data, return breed not found
+      if (data.length > 0) {
+        console.log(data[0].description);
+      } else {
+        console.log("Breed not found");
+      }
     }
   });
 };
 
-breedfetcher();
+//use process.argv to take in beed name as command line argument
+const args = process.argv.slice(2);
+if (args.length === 0) {
+  console.log("Please provide a breed name");
+} else {
+  breedFetcher(args);
+}
